8410330863d3511e06afe438eefce12111e218b1
Rodrigo Rigo <rodrigo.rigo@outlook.com.br>
2025-04-12 10:11:26 -0300

    adicionando mais verificações em relação aos argumentos que serão passados por linha de comando

--- PedroRafaelDomenighi_RodrigoRigo.c
+++ PedroRafaelDomenighi_RodrigoRigo.c
@@ -13,6 +13,19 @@ int main (int argc, char *argv[] ) {
        exit(EXIT_FAILURE);
     }
 
+    if (atoi(argv[1]) != 2 || atoi (argv[1]) != 8 || atoi (argv[1]) || atoi (argv[1]) != 16){
+        fprintf(stderr, "Erro: FORMATO_SAIDA deve ser 2, 8, 10 ou 16.\n");
+        exit(EXIT_FAILURE);
+    }
+    if (atoi (argv[2]) <= 0){
+        fprintf(stderr, "Erro: NUM_LEITURAS deve ser um inteiro positivo.");
+        exit(EXIT_FAILURE);
+    }
+    if (atoi (argv[3]) <= 0 ) {
+        fprintf(stderr, "Erro: NUM_LEITURAS deve ser um inteiro positivo.");
+        exit(EXIT_FAILURE);
+    }
+
     
 
 
