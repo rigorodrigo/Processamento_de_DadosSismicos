9e7617f2ce7080394e051e265c4e4770cd5d638b
Rodrigo Rigo <rodrigo.rigo@outlook.com.br>
2025-04-12 09:57:58 -0300

    iniciando com adição de verificação se a quantidade de argumentos é válida

--- PedroRafaelDomenighi_RodrigoRigo.c
+++ PedroRafaelDomenighi_RodrigoRigo.c
@@ -3,11 +3,17 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+#include <stdlib.h>
 
 
-int main () {
+int main (int argc, char *argv[] ) {
 
-    int FORMATO_SAIDA, NUM_LEITURAS, TEMPO_LIMITE;
+    if (argc != 4) {
+       fprintf(stderr, "Uso: %s <FORMATO_SAIDA> <NUM_LEITURAS> <TEMPO_KIMITE>", argv[0]);
+       exit(EXIT_FAILURE);
+    }
+
+    
 
 
     return 0;
