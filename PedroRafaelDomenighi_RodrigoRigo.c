404e70e9c93d1680f1987e5079aa2901624d266f
Rodrigo Rigo <rodrigo.rigo@outlook.com.br>
2025-04-18 10:01:27 -0300

    Ajustes nas implementações anteriores, adicionando functions para print e criando o processo gerador

--- PedroRafaelDomenighi_RodrigoRigo.c
+++ PedroRafaelDomenighi_RodrigoRigo.c
@@ -9,45 +9,96 @@
 #define AZUL   "\033[34m"
 #define RESET  "\033[0m"
 
+int pipefd [2], formato_saida,num_leituras,tempo_limite;
+pid_t pid_esr, pid_cpg;
+
 void trata_SIGINT(int sig) {
     printf(AZUL "[CPG PID:%d] Análise interrompida pelo pesquisador!\n" RESET, getpid());
-    close(pipe_fd[0]);
+    close(pipefd[0]);
     exit(EXIT_FAILURE);
 }
 
 void trata_SIGALRM(int sig) {
     printf(AZUL "[CPG PID:%d] Processamento excedeu o tempo limite!\n" RESET, getpid());
-    close(pipe_fd[0]);
+    close(pipefd[0]);
     exit(EXIT_FAILURE);
+}
+
+void printa_numero_convertido (unsigned short n){
+    switch (formato_saida)
+    {
+    case 2:
+        printf("Formato 2: ");
+        for (int i = 15; i >= 0 ; i --){
+            printf("%d", (n >> i) & 1);
+        }
+        printf("\n");
+        break;
+    
+    case 8: 
+        printf("Formato 8: %o\n", n);
+        break;
+
+    case 10: 
+        printf("Formato 10: %u\n", n);
+        break;
+    
+    case 16: 
+        printf("Formato 16: %X\n", n);
+        break;
+
+    default:
+        fprintf(stderr, "Erro: FORMATO_SAIDA deve ser 2, 8, 10 ou 16.\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void printa_numero_binario(unsigned short n){
+    for (int i = 15; i >= 0 ; i --){
+        printf("%d", (n >> i) & 1);
+    }
 }
 
 
 int main (int argc, char *argv[] ) {
 
+    formato_saida = atoi(argv[1]);
+    num_leituras = atoi(argv[2]);
+    tempo_limite = atoi(argv[3]);
+
     if (argc != 4) {
-       fprintf(stderr, "Uso: %s <FORMATO_SAIDA> <NUM_LEITURAS> <TEMPO_KIMITE>", argv[0]);
+       fprintf(stderr, "Uso: %s <FORMATO_SAIDA> <NUM_LEITURAS> <TEMPO_LIMITE>", argv[0]);
        exit(EXIT_FAILURE);
     }
 
-    if (atoi(argv[1]) != 2 || atoi (argv[1]) != 8 || atoi (argv[1]) || atoi (argv[1]) != 16){
+    if (formato_saida != 2 && formato_saida != 8 && formato_saida != 10 && formato_saida != 16){
         fprintf(stderr, "Erro: FORMATO_SAIDA deve ser 2, 8, 10 ou 16.\n");
         exit(EXIT_FAILURE);
     }
-    if (atoi (argv[2]) <= 0){
+    if (num_leituras <= 0){
         fprintf(stderr, "Erro: NUM_LEITURAS deve ser um inteiro positivo.");
         exit(EXIT_FAILURE);
     }
-    if (atoi (argv[3]) <= 0 ) {
+    if (tempo_limite <= 0 ) {
         fprintf(stderr, "Erro: NUM_LEITURAS deve ser um inteiro positivo.");
         exit(EXIT_FAILURE);
     }
 
-    int pipefd [2];
-    pipe(pipefd);
+    if (pipe (pipefd) == -1) {
+        perror("erro no pipe");
+        exit(EXIT_FAILURE);
+    }
 
-    pid_t pid_esr = fork();
+    pid_esr = fork();
 
+    if (pid_esr < 0) {
+        perror ("erro ao criar processo filho");
+        exit(EXIT_FAILURE);
+    }
 
+    if (pid_esr == 0) {
+        close(pipefd[0]);   // fechando a extremidade de leitura do pipe
+    }
 
     return 0;
 }
